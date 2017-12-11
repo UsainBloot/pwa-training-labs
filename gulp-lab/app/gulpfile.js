8ea01cd4174e5c6ad0babbbfcd3d786f1dca460f
Jack Palmer <jack.palmer@bbc.co.uk>
2017-12-11 16:04:57 +0000

    Gulp Lab

--- gulp-lab/app/gulpfile.js
+++ gulp-lab/app/gulpfile.js
@@ -1,11 +1,34 @@
-// TODO 3 - include gulp
-// TODO 4.1 - include gulp-uglify
-// TODO 6.3a - include browserSync
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
 
-// TODO 4.2 - uglify / minify JavaScript
+gulp.task('minify', function() {
+  gulp.src('js/main.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('build'));
+});
 
-// TODO 6.1 - add default tasks
+gulp.task('processCSS', function() {
+  gulp.src('styles/main.css')
+  .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+  }))
+  .pipe(gulp.dest('build'))
+});
 
-// TODO 6.2 - watch files
+gulp.task('default', ['serve']);
 
-// TODO 6.3b - run a local server
+gulp.task('watch', function() {
+  gulp.watch('styles/*.css', ['processCSS']);
+});
+
+gulp.task('serve', ['processCSS'], function() {
+  browserSync.init({
+    server: '.',
+    port: 3000
+  });
+  gulp.watch('styles/*.css', ['processCSS']).on('change', browserSync.reload);
+  gulp.watch('*.html').on('change', browserSync.reload);
+});

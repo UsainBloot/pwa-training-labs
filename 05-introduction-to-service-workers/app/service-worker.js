3d4c40fd28036b9459947b83088a0de7d889c7a0
Jack Palmer <jack.palmer@bbc.co.uk>
2017-12-11 14:39:16 +0000

    Service Worker Lab

--- 05-introduction-to-service-workers/app/service-worker.js
+++ 05-introduction-to-service-workers/app/service-worker.js
@@ -3,7 +3,7 @@
     //5) Listening for life cycle events
     //The service worker emits an install event at the end of registration.
     //We log a message, but this is a good place for caching static assets.
-    self.addEventListener('', function (event) {
+    self.addEventListener('install', function (event) {
         console.log('Service worker installing...');
 
         //8) It is possible for a new service worker to activate immediately, even if an existing service
@@ -17,9 +17,9 @@
     //Only one service worker can be active at a time for a given scope
     //so a newly installed service worker isn't activated until the existing service
     //worker is no longer in use.
-    //This is why all pages controlled by a service worker must be closed before a 
+    //This is why all pages controlled by a service worker must be closed before a
     //new service worker can take over.
-    self.addEventListener('', function (event) {
+    self.addEventListener('activate', function (event) {
         console.log('Service worker activating...');
     });
 
@@ -36,12 +36,12 @@
     //9) Intercept network requests
     //Service Workers can act as a proxy between your web app and the network.
     //The service worker receives a fetch event for every HTTP request made by the browser.
-    //fetch event object contains the request; 
-    //Listening for fetch events in the service worker is similar to listening to click events 
+    //fetch event object contains the request;
+    //Listening for fetch events in the service worker is similar to listening to click events
     //in the DOM.
     //when a fetch event occurs, we log the requested URL to the console.
 
-    self.addEventListener('', function (event) {
+    self.addEventListener('fetch', function (event) {
         console.log('Fetching:', event.request.url);
     });
 
--- service-worker-lab/app/service-worker.js
+++ service-worker-lab/app/service-worker.js
@@ -15,11 +15,19 @@ limitations under the License.
 */
 (function() {
   'use strict';
+  // I'm a new service worker
 
-  // TODO - 3.1: Add install and activate event listeners
+  self.addEventListener('install', function(event) {
+    console.log('Service worker installing...');
+    self.skipWaiting();
+  });
 
-  // TODO - 3.3: Add a comment to change the service worker
+  self.addEventListener('activate', function(event) {
+    console.log('Service worker activating...');
+  });
 
-  // TODO - 4: Add fetch listener
+  self.addEventListener('fetch', function(event) {
+  console.log('Fetching:', event.request.url);
+});
 
 })();
